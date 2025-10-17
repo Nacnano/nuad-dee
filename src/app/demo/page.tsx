a20d476143da698798a1b19c91fd0824dddba9c9
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-10-17 08:05:58 +0700

    fix: remove to have just one tutor component

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -10,8 +10,8 @@ export default function DemoPage() {
   return (
     <>
       {/* Client-side Message Tutor (Press Start Session, and try speaking to it) */}
-      <AIStudioMassageTutor />
-      Thinking Mode Disabled
+      {/* <AIStudioMassageTutor /> */}
+      {/* Thinking Mode Disabled */}
       <NoThinkingAIStudioMassageTutor />
       Skeleton Demo (Wait 5-10 seconds to load model)
       <RealtimePostureAnalysis />
